db72372cd56145475c3bb0419e2c88608673764c
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-02-01 22:13:04 +0330

    Update and rename seedbot.lua to persianguard.lua

--- bot/persianguard.lua
+++ bot/persianguard.lua
@@ -226,20 +226,16 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,111020322,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {150575718,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/hfjgjfg/persianguard
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
+@mohammad20162015
 Special thanks to
 awkward_potato
 Siyanew
@@ -247,8 +243,8 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@ [English]
+@ [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -326,7 +322,7 @@ This command will send text to [group_id]
 ]],
     help_text = [[
 Commands list :
-
+از انتخاب شما متشکریم 
 !kick [username|id]
 You can also do it by reply
 
@@ -429,6 +425,7 @@ will return group ban list
 *Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 
 *Only owner can use res,setowner,promote,demote and log commands
+
 
 ]]
   }
